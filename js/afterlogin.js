656ff569010699fa38906f60d4652dfabea893ef
CHIHIRO KATO <chihiro@CHIHIROnoMacBook-Pro.local>
2020-10-08 19:38:11 -0700

    added js

--- js/afterlogin.js
+++ js/afterlogin.js
@@ -9,6 +9,17 @@ onload = function() {
     const userSpan =  document.querySelector('.username span');
     userSpan.textContent = `Welcome! ${username}`;
 
-    //check the role to get access to the nav-item
-
+    const logOutButton = document.getElementById("logOutBtn");
+    logOutButton.addEventListener("click", () =>{
+        var date1 = new Date();
+        //1970年1月1日00:00:00の日付データをセットする
+        date1.setTime(0);
+        //有効期限を過去にして書き込む
+        document.cookie = "counts=;expires="+date1.toGMTString();
+        //ページを再読み込みする
+        location.reload();
+        })
     }
+
+
+    //check the role to get access to the nav-item
