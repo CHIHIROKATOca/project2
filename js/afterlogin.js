57165b09adb84a2e16237895fb3602941c6b2cb2
CHIHIRO KATO <chihiro@CHIHIROnoMacBook-Pro.local>
2020-10-10 00:23:30 -0700

    added js

--- js/afterlogin.js
+++ js/afterlogin.js
@@ -1,15 +1,12 @@
 onload = function() {
+
     //split data from cookie
     const cookieInfo = document.cookie;
 
     const cookieArray = document.cookie.split(';');
-    const username = cookieArray[0].split('=')[1].toUpperCase();
+    const username = cookieArray[0].split('=')[1];
 
     //add the data from cookie to html
     const userSpan =  document.querySelector('.username span');
     userSpan.textContent = `Welcome! ${username}`;
-
     }
-
-
-    //check the role to get access to the nav-item
