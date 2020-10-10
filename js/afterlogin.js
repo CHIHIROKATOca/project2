4449113120495715383139f9e7a1af4e1e87f5de
CHIHIRO KATO <chihiro@CHIHIROnoMacBook-Pro.local>
2020-10-10 00:36:21 -0700

    added js

--- js/afterlogin.js
+++ js/afterlogin.js
@@ -2,11 +2,9 @@ onload = function() {
 
     //split data from cookie
     const cookieInfo = document.cookie;
-
     const cookieArray = cookieInfo.split(';');
     const username = cookieArray[0].split('=')[1].toLowerCase();
-
     //add the data from cookie to html
-    const userSpan =  document.querySelector('.username span');
+    const userSpan =  document.querySelector('.username-login span');
     userSpan.textContent = `Welcome! ${username}`;
     }
